f1e4cf7081364449d1364f8518d93b25a733bd97
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-18 17:38:20 -0300

    feat: criado entity Food

---
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,22 @@
+package com.example.cardapio.food;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+}
