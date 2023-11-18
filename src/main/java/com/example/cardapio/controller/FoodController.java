857c7e2e6ff3b7f254efe90c2cd807de05e960b9
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-18 17:39:12 -0300

    feat: criado controller e DTO

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,25 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll() {
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
---
+++ src/main/java/com/example/cardapio/food/FoodRepository.java
@@ -0,0 +1,5 @@
+package com.example.cardapio.food;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {}
