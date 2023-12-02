43becfa77bdc6561e39d08c8c22dd5f1f873c3b2
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-12-01 22:49:34 -0300

    refactor(Drink): adicionado status nos retornos dos endpoints

--- src/main/java/com/example/cardapio/controller/DrinkController.java
+++ src/main/java/com/example/cardapio/controller/DrinkController.java
@@ -4,9 +4,11 @@ import com.example.cardapio.drink.Drink;
 import com.example.cardapio.drink.DrinkResponseDTO;
 import com.example.cardapio.drink.DrinkRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/drink")
@@ -14,38 +16,42 @@ public class DrinkController {
     @Autowired
     private DrinkRespository repository;
 
+
     @GetMapping
-    public List<DrinkResponseDTO> getAll() {
-        return repository
+    public ResponseEntity<List<DrinkResponseDTO>> getAll() {
+        List<DrinkResponseDTO> drinkList = repository
                 .findAll()
                 .stream()
                 .map(DrinkResponseDTO::new)
-                .toList();
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().body(drinkList);
     }
 
     @GetMapping("/{id}")
-    public DrinkResponseDTO getDrink(@PathVariable Long id) {
+    public ResponseEntity<Drink> getDrink(@PathVariable Long id) {
         Drink drink = repository.findById(id).orElseThrow();
-        return new DrinkResponseDTO(drink);
+        return ResponseEntity.ok().body(drink);
     }
 
     @PostMapping
-    public Drink saveDrink(@RequestBody DrinkResponseDTO data) {
+    public ResponseEntity<Drink> saveDrink(@RequestBody DrinkResponseDTO data) {
         Drink drink = new Drink(data);
         repository.save(drink);
-        return drink;
+        return ResponseEntity.status(201).body(drink);
     }
 
     @PutMapping("/{id}")
-    public Drink updateDrink(@PathVariable Long id, @RequestAttribute DrinkResponseDTO data) {
+    public ResponseEntity<Void> updateDrink(@PathVariable Long id, @RequestBody DrinkResponseDTO data) {
         Drink drink = repository.findById(id).get();
         drink.setData(data);
         repository.save(drink);
-        return drink;
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
-    public void deleteDrink(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteDrink(@PathVariable Long id) {
         repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
